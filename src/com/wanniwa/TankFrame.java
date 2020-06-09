c3e01e76b75f28315d6dd3589072ee805af00d0b
wangning <wangning1@yunzhangfang.com>
2020-06-09 17:19:34 +0800

    添加键盘监听，按下后，移动方块

--- src/com/wanniwa/TankFrame.java
+++ src/com/wanniwa/TankFrame.java
@@ -1,32 +1,56 @@
 package com.wanniwa;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
-    int x,y =10;
+    int x,y =50;
     public TankFrame(){
         this.setSize(800, 600);
         this.setResizable(false);
         this.setTitle("tank war");
         this.setVisible(true);
 
-        addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 super.windowClosing(e);
                 System.exit(0);
             }
         });
+
+        this.addKeyListener(new KeyHandler());
     }
 
     @Override
     public void paint(Graphics g) {
         System.out.println("paint");
         g.fillRect(x,y,50,50);
-        x+=50;
-        y+=50;
-        g.setColor(Color.lightGray);
+
+    }
+
+    class KeyHandler implements KeyListener {
+
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("按下");
+            x+=10;
+            y+=10;
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("keyReleased");
+        }
     }
 }
--- src/com/wanniwa/tank/T.java
+++ src/com/wanniwa/tank/T.java
@@ -7,7 +7,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class T {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TankFrame tankFrame = new TankFrame();
     }
 }
